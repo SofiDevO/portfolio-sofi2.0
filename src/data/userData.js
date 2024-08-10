71f7af1001c0eba99de9ea4591261ffcb453a887
SofiDev <angiemeow89@gmail.com>
2024-08-09 22:16:53 -0600

    🚧 Section about

--- src/data/userData.js
+++ src/data/userData.js
@@ -5,6 +5,9 @@ export const userData = {
     sloganHero1: "Frontend Developer in Mexico",
     profilePicture: "../img/sofi-pic-1.webp",
     profilePicture2: "../img/sofi-pic-2.webp",
+    aboutPicture: "../img/about-sofi.jpeg",
     logo: "../img/icons/sofi-icon.png",
-    about: "Me llamo Ángela Sofía Osorio. Soy desarrolladora web con 1 año y medio de experiencia. <br><br> Puedes encontrarme en mis redes sociales  ",
+    about: "Me llamo Ángela Sofía Osorio. Soy desarrolladora web con 1 año y medio de experiencia. \n Puedes encontrarme en mis redes sociales  ",
+    cv:"../docs/Angela_Sofia_Osorio_CV2024_updated.pdf",
+    email:"sofi.dev@outlook.com"
 };
