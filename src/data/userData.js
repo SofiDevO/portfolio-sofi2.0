744c6654166a79f743761914a99f76c19db149a8
SofiDev <angiemeow89@gmail.com>
2024-08-26 12:44:36 -0600

    🚧 Se agrega responsive, se modifican textos

--- src/data/userData.js
+++ src/data/userData.js
@@ -1,4 +1,5 @@
 export const userData = {
+    logo: "../img/icons/sofi-icon.png",
     name: "Sofi",
     lastName: "Dev",
     userName: "SofiDev",
@@ -6,8 +7,17 @@ export const userData = {
     profilePicture: "../img/sofi-pic-1.webp",
     profilePicture2: "../img/sofi-pic-2.webp",
     aboutPicture: "../img/sofi-selfie.png",
-    logo: "../img/icons/sofi-icon.png",
-    about: "Frontend Designer/Developer with experience in designing and developing attractive and functional user interfaces. \n \n Proficient in a variety of web technologies including CSS, Sass, JavaScript, React, WordPress, and more. With a solid foundation in Data Entry and analytical skills.",
-    cv:"../docs/Angela_Sofia_Osorio_CV2024_updated.pdf",
-    email:"sofi.dev@outlook.com"
+    about: {
+        intro: "Javascript Fan 😍. Always looking for new skills to learn. \n",
+        highlighted: "What sets me apart?",
+        continuation: "I genuinely love what I do! 💙 I thrive on transforming  ideas into unique digital solutions. 💡 \n\n  My focus goes beyond coding—it's about creating experiences that captivate and solve real-world problems in innovative ways. 🚀"
+    },
+    aboutSkills: {
+        intro: "Javascript Fan 😍. Always looking for new skills to learn. \n",
+        highlighted: "What sets me apart?",
+        continuation: "I genuinely love what I do! 💙 I thrive on transforming  ideas into unique digital solutions. 💡 My focus goes beyond coding—it's about creating experiences that captivate and solve real-world problems in innovative ways. 🚀"
+    },
+    terminalInfo: "Frontend Designer/Developer with experience in designing and developing attractive and functional user interfaces. \n\nProficient in a variety of web technologies including CSS, Sass, JavaScript, React, WordPress, and more. With a solid foundation in Data Entry and analytical skills.",
+    cv: "../docs/Angela_Sofia_Osorio_CV2024_updated.pdf",
+    email: "sofi.dev@outlook.com"
 };
