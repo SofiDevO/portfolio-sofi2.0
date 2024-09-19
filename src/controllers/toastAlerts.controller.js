f03c0b9e441e32f4f8022669c01b6d1a3a1a6c89
SofiDev <angiemeow89@gmail.com>
2024-09-19 04:01:30 -0600

    ✨ Toast notifications

---
+++ src/controllers/toastAlerts.controller.js
@@ -0,0 +1,43 @@
+const toastBox = document.querySelector(".toast-alerts");
+
+export default function ToastNotification(msg, type = "info") {
+  if (!toastBox) {
+    console.error("Toast container not found.");
+    return;
+  }
+
+  let alertType;
+
+  switch (type) {
+    case "success":
+      alertType = "success";
+
+      break;
+    case "error":
+      alertType = "error";
+      break;
+    case "warning":
+      alertType = "warning";
+      break;
+    default:
+      alertType = "info";
+  }
+
+
+
+  // Create the toast
+  const toast = document.createElement("div");
+  toast.classList.add("toast-message", alertType);
+  toast.innerHTML = msg;
+
+  // Append the toast to the toastBox
+  toastBox.appendChild(toast);
+  toastBox.classList.add("show");
+
+
+const toastMessages = document.querySelectorAll('.toast-message');
+  // Eliminar el toast después de 5 segundos
+  setTimeout(() => {
+      toast.remove();
+  }, 5000);
+}
