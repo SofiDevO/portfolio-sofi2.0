0c2d725f268d23640f9761b27575eccecc0017a4
SofiDev <angiemeow89@gmail.com>
2024-09-27 06:49:49 -0600

    hotFix

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import react from "@astrojs/react";
 
 import node from "@astrojs/node";
 
-import vercel from "@astrojs/vercel";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
