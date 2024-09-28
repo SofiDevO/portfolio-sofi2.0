36da06302370c54352e0a079bb3d1d91f5f94c41
SofiDev <angiemeow89@gmail.com>
2024-09-27 21:45:20 -0600

    vercel output deleted

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig, envField } from "astro/config";
 
 import react from "@astrojs/react";
 
-import node from "@astrojs/node";
+// import node from "@astrojs/node";
 
 import vercel from "@astrojs/vercel/serverless";
 
