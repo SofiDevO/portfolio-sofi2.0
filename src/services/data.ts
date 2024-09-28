a7adf5595f53f4137dcc8da5a65efb6050133b2b
SofiDev <angiemeow89@gmail.com>
2024-09-27 22:10:58 -0600

    vercel output deleted

--- src/services/data.ts
+++ src/services/data.ts
@@ -2,7 +2,10 @@
 // Si estamos en un entorno de desarrollo (local), usamos 'http://localhost:4322'.
 // Si no estamos en desarrollo (producción), usamos 'import.meta.env.SITE_URL'.
 const localUrl = "http://localhost:4322" ;
-const url = localUrl ? localUrl : import.meta.env.SITE_URL;
+ const url = !localUrl ?  import.meta.env.SITE_URL :localUrl ;
+// const url = "https://itssofi.dev/";
+
+
 
 // Creamos una función asíncrona que toma un argumento opcional 'dataType'.
 // Por defecto, 'dataType' es "data" si no se proporciona otro valor.
