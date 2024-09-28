0a11f514fc330f98d01342d3743b0636b787b36b
Ricardo Vega G <rikytoloser@gmail.com>
2024-09-28 03:14:50 -0600

    🧽 Se creo el formateo de la api del YouTube para los videos

--- src/services/youtubeLatestVid.js
+++ src/services/youtubeLatestVid.js
@@ -1,12 +1,12 @@
 export const fetchYouTubeVideos = async (channelId) => {
-  const API_key = import.meta.env.YT_API ? import.meta.env.YT_API : "";
+  const API_key = import.meta.env.YT_API
+    ? import.meta.env.YT_API
+    : "&api_key=wmbjfv0ybbzjsaceaojxrbm9aaelzzq2bbe7mgws";
 
   const channelURL = encodeURIComponent(
     `https://www.youtube.com/feeds/videos.xml?channel_id=${channelId}`
   );
   const reqURL = `https://api.rss2json.com/v1/api.json?rss_url=${channelURL}&api_key=${API_key}`;
-
-  console.log(reqURL);
 
   try {
     const response = await fetch(reqURL);
