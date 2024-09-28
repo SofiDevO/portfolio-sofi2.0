fe304094973e883fc68918425edb775af563a521
SofiDev <angiemeow89@gmail.com>
2024-09-27 22:47:16 -0600

    vercel output deleted

--- src/services/data.ts
+++ src/services/data.ts
@@ -1,12 +1,12 @@
 // Definimos una variable 'url' que se usará para la solicitud a la API.
 // Si estamos en un entorno de desarrollo (local), usamos 'http://localhost:4322'.
 // Si no estamos en desarrollo (producción), usamos 'import.meta.env.SITE_URL'.
-import { SITE_URL} from 'astro:env/client';
+// import { SITE_URL} from 'astro:env/client';
 
- const localUrl = "http://localhost:4322" ;
-const url = localUrl ?  localUrl : SITE_URL;
-console.log( "🐛", SITE_URL )
-// const url = "https://itssofi.dev/";
+//  const localUrl = "http://localhost:4322" ;
+// const url = localUrl ?  localUrl : SITE_URL;
+// console.log( "🐛", SITE_URL )
+const url = "https://itssofi.dev/";
 
 
 
