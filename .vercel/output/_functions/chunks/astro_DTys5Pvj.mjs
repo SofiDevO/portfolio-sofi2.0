cecedaf5443f6051fc9a551a059b9cf55e75e04c
SofiDev <angiemeow89@gmail.com>
2024-09-27 20:39:40 -0600

    change url

---
+++ .vercel/output/_functions/chunks/astro_DTys5Pvj.mjs
@@ -0,0 +1,3 @@
+import 'kleur/colors';
+import './astro/server_CJ5Zltcn.mjs';
+import 'clsx';
--- .vercel/output/_functions/entry.mjs
+++ .vercel/output/_functions/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports } from './chunks/entrypoint_DC6CokKx.mjs';
-import { manifest } from './manifest_5hTfEAqr.mjs';
+import { manifest } from './manifest_D_yVl0fn.mjs';
 
 const _page0 = () => import('./pages/_image.astro.mjs');
 const _page1 = () => import('./pages/about.astro.mjs');
@@ -25,7 +25,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "fe2d4c35-002d-41a8-8991-c3886fe56f7b",
+    "middlewareSecret": "75693ac1-af5c-44d0-8568-d8be4d9fd6e2",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
