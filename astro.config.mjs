c7383d4b3bb2a42a343bf926077c9b96e55bcc90
SofiDev <angiemeow89@gmail.com>
2024-09-27 20:03:34 -0600

    change url

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ export default defineConfig({
   site: "https://itssofi.dev/",
   integrations: [react()],
   output: "server",
-  adapter: vercel(),
+  adapter: node({mode: 'standalone',}),
   experimental: {
     env: {
       schema: {
