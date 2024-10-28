cce27172525233aac6232bc50b558779be122d17
SofiDev <angiemeow89@gmail.com>
2024-10-28 03:18:19 -0600

    ✨ Se corrige metadescription

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,2 @@
-export const SITE_TITLE = "Sofi Dev";
-export const SITE_DESCRIPTION = "Frontend Developer";
+export const SITE_TITLE = "SofiDev";
+export const SITE_DESCRIPTION = "Ángela sofía Osorio |  Javaacript  Developer";
