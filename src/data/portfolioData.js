20b866f7e97a172d9b955b4d9b592845ebd42de2
SofiDev <angiemeow89@gmail.com>
2024-10-27 22:31:39 -0600

    💄 Cosas

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -2,9 +2,9 @@ const portafolioData = [
   {
     imgSrc:
       "https://cdn.outsideonline.com/wp-content/uploads/2023/03/Funny_Dog_H.jpg?crop=16:9&width=960&enable=upscale&quality=100",
-    titulo: "Alura Flix",
+    title: "Alura Flix",
     skills: ["React", "StyledComponents"],
-    descripcion:
+    description:
       "Final project for the One Oracle Next Education Program. Educational programming video website demonstrating CRUD requests.",
     demoURL: "https://alura-flix-self.vercel.app/",
     repoURL: "https://github.com/SofiDevO/alura-flix",
@@ -13,9 +13,9 @@ const portafolioData = [
   {
     imgSrc:
       "https://cdn.outsideonline.com/wp-content/uploads/2023/03/Funny_Dog_H.jpg?crop=16:9&width=960&enable=upscale&quality=100",
-    titulo: "Alura Geek",
+    title: "Alura Geek",
     skills: ["JavaScript", "CSS"],
-    descripcion: "Final project for the One Oracle Next Education Program. Educational programming video website demonstrating CRUD requests.",
+    description: "Final project for the One Oracle Next Education Program. Educational programming video website demonstrating CRUD requests.",
     demoURL: "https://alura-geek-ruddy.vercel.app/",
     repoURL: "https://github.com/SofiDevO/alura-geek",
     anim: "fade-up",
@@ -23,9 +23,9 @@ const portafolioData = [
   {
     imgSrc:
       "https://cdn.outsideonline.com/wp-content/uploads/2023/03/Funny_Dog_H.jpg?crop=16:9&width=960&enable=upscale&quality=100",
-    titulo: "React ORG",
+    title: "React ORG",
     skills: ["React", "CSS"],
-    descripcion: "Final project for the One Oracle Next Education Program. Educational programming video website demonstrating CRUD requests.",
+    description: "Final project for the One Oracle Next Education Program. Educational programming video website demonstrating CRUD requests.",
     demoURL: "https://react-org-delta.vercel.app/",
     repoURL: "https://github.com/SofiDevO/react-org",
     anim: "fade-left",
@@ -33,9 +33,9 @@ const portafolioData = [
   {
     imgSrc:
       "https://cdn.outsideonline.com/wp-content/uploads/2023/03/Funny_Dog_H.jpg?crop=16:9&width=960&enable=upscale&quality=100",
-    titulo: "My Portfolio",
+    title: "My Portfolio",
     skills: ["Astro", "React", "JavaScript"],
-    descripcion: "Final project for the One Oracle Next Education Program. Educational programming video website demonstrating CRUD requests.",
+    description: "Final project for the One Oracle Next Education Program. Educational programming video website demonstrating CRUD requests.",
     demoURL: "https://sofidev-portfolio-astro-delta.vercel.app/",
     repoURL: "https://github.com/SofiDevO/sofidev-portfolio-astro",
     anim: "fade-right",
@@ -43,9 +43,9 @@ const portafolioData = [
   {
     imgSrc:
       "https://cdn.outsideonline.com/wp-content/uploads/2023/03/Funny_Dog_H.jpg?crop=16:9&width=960&enable=upscale&quality=100",
-    titulo: "Eco Store",
+    title: "Eco Store",
     skills: ["Sass", "JavaScript"],
-    descripcion: "Made with sass for the,Sass fundamentals course",
+    description: "Made with sass for the,Sass fundamentals course",
     demoURL: "https://sofidevo.github.io/eco-store-sass/",
     repoURL: "https://github.com/SofiDevO/eco-store-sass",
     anim: "fade-up",
@@ -53,9 +53,9 @@ const portafolioData = [
   {
     imgSrc:
       "https://cdn.outsideonline.com/wp-content/uploads/2023/03/Funny_Dog_H.jpg?crop=16:9&width=960&enable=upscale&quality=100",
-    titulo: "fruto & Fruta",
+    title: "fruto & Fruta",
     skills: ["Bootstrap", "JavaScript"],
-    descripcion: "Made with Bootstrap for the, Bootstrap  course",
+    description: "Made with Bootstrap for the, Bootstrap  course",
     demoURL: "https://sofidevo.github.io/bootstrap-curso/",
     repoURL: "https://github.com/SofiDevO/bootstrap-curso",
     anim: "fade-left",
@@ -64,9 +64,9 @@ const portafolioData = [
   {
     imgSrc:
       "https://cdn.outsideonline.com/wp-content/uploads/2023/03/Funny_Dog_H.jpg?crop=16:9&width=960&enable=upscale&quality=100",
-    titulo: "Message Encryptor",
+    title: "Message Encryptor",
     skills: ["JavaScript", "CSS"],
-    descripcion:
+    description:
       "Incididunt amet proident id elit id excepteur anim ullamco cillum eiusmod esse consequat veniam. Eu reprehenderit quis deserunt ea non deserunt dolor consequat fugiat. \n\n Consectetur deserunt dolor do dolore occaecat reprehenderit ipsum ex.",
     demoURL: "https://sofidevo.github.io/encriptador-mensajes/",
     repoURL: "https://github.com/SofiDevO/encriptador-mensajes",
@@ -82,7 +82,14 @@ const skillIcons = {
   Sass: "skill-icons:sass",
   StyledComponents: "skill-icons:styledcomponents",
   Bootstrap: "devicon:bootstrap",
-  /*  Tailwind: "skill-icons:tailwindcss-dark", */
+  Tailwind: "skill-icons:tailwindcss-dark",
+  NodeJs: "skill-icons:nodejs-dark",
+  Express: "skill-icons:expressjs-dark",
+  MySQL: "skill-icons:mysql-dark",
+  Wordpress: "skill-icons:wordpress",
+  HTML: "skill-icons:html",
+  Vue: "skill-icons:vuejs-dark",
+
 };
 const skillsIconsMapped = portafolioData.map((item) => {
   return {
