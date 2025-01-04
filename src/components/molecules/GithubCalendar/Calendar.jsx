cdeb61915a757374c8e9eabe41c497d236227b12
SofiDev <angiemeow89@gmail.com>
2025-01-04 02:11:15 -0600

    🚧 Seccion contributtor

--- src/components/molecules/GithubCalendar/Calendar.jsx
+++ src/components/molecules/GithubCalendar/Calendar.jsx
@@ -3,21 +3,15 @@ import { Tooltip as MuiTooltip } from "@mui/material";
 import GitHubCalendar from "react-github-calendar";
 import YearButton from "@atoms/YearButton/YearButton";
 
-
-
-
 function Calendar() {
   const [selectedYear, setSelectedYear] = useState(new Date().getFullYear());
   const [activeButton, setActiveButton] = useState(new Date().getFullYear());
   const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
-  
+
   const handleYearButtonClick = (year) => {
     setSelectedYear(year);
     setActiveButton(year);
   };
-
-
-
 
   return (
     <>
@@ -28,12 +22,17 @@ function Calendar() {
             year={selectedYear}
             weekStart={1}
             theme={{
-              light: ["hsl(0, 0%, 9.411764705882353%)", "hsl(263.1034482758621, 72.5%, 52.94117647058824%)"],
-              dark: ["hsl(0, 0%, 16.07843137254902%)", "hsl(263.1034482758621, 72.5%, 52.94117647058824%)"],
+              light: [
+                "hsl(0, 0%, 9.411764705882353%)",
+                "hsl(263.1034482758621, 72.5%, 52.94117647058824%)",
+              ],
+              dark: [
+                "hsl(0, 0%, 16.07843137254902%)",
+                "hsl(263.1034482758621, 72.5%, 52.94117647058824%)",
+              ],
             }}
             renderBlock={(block, activity) => (
               <MuiTooltip
-
                 title={`${activity.count} activities on ${activity.date}`}
               >
                 {block}
@@ -42,7 +41,7 @@ function Calendar() {
           />
         </div>
         <div className="year__buttons">
-          {[currentYear , currentYear -1, currentYear -2].map((year) => (
+          {[currentYear, currentYear - 1, currentYear - 2].map((year) => (
             <YearButton
               key={year}
               year={year}
