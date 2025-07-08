be1b6283a83c55b826e7bd3b972b192428b9f210
SofiDev <angiemeow89@gmail.com>
2025-07-08 04:30:13 -0600

    🐛 Fix image import problem

--- src/data/userData.js
+++ src/data/userData.js
@@ -7,7 +7,7 @@ export const userData = {
   profilePicture: "/img/raw.png",
   profilePicture2: "/img/raw.png",
   aboutPicture:
-    "https://res.cloudinary.com/dvjox4xut/images/v1740707687/shophi/shophi.png",
+    "/img/shophi.png",
   aboutSkills: {
     intro:
       "Skilled in modern web technologies, I create dynamic and user-centric digital solutions. \n\nMy expertise in both front-end and back-end development allows for the seamless integration of design and functionality, ensuring impactful and engaging applications.\n",
