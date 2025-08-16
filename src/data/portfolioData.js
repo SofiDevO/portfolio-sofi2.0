ac7efcbf55aa05d0534788c350bc80f189a9274a
SofiDev <angiemeow89@gmail.com>
2025-08-15 23:00:25 -0600

    🐛 Se elimina export duplicado

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -1,4 +1,4 @@
-export const portafolioData = [
+ const portafolioData = [
   {
     imgSrc:
       "https://cdn.outsideonline.com/wp-content/uploads/2023/03/Funny_Dog_H.jpg?crop=16:9&width=960&enable=upscale&quality=100",
